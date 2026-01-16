2ae6904ad22af29d8f889215781f1227f7131454
Sota-Takeda <220647849+Sota-Takeda@users.noreply.github.com>
2026-01-16 13:09:33 +0900

    feat: protect books routes with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,16 @@ use App\Http\Controllers\TestController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\BookController;
 
-Route::resource('books', BookController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/test', [TestController::class, 'index']);
 
 Route::resource('posts', PostController::class)->except(['show']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::resource('books', BookController::class);
 });
+
+require __DIR__.'/auth.php';
