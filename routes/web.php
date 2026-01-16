03086be172ba14eba99fba5fbf54d260b43b6047
Sota-Takeda <220647849+Sota-Takeda@users.noreply.github.com>
2026-01-16 13:22:44 +0900

    feat: add auth routes and protect books/dashboard/profile

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,14 @@ Route::resource('posts', PostController::class)->except(['show']);
 
 Route::middleware('auth')->group(function () {
     Route::resource('books', BookController::class);
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
